a33fb7b742a577f3be430d464e075c82fe113cbd
egor dorosenko <dorohenkojord@gmail.com>
2022-04-19 16:27:48 +0300

    auth jwt complit completed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { GetUser } from 'src/auth/decoretor';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe() {
-    return 'info';
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
